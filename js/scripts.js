2286a5d85eaf547347f2bb3affb611f1a1171850
unknown <bogdan_walle@yahoo.com>
2022-12-18 23:37:21 +0200

    index changes v5

--- js/scripts.js
+++ js/scripts.js
@@ -238,10 +238,10 @@ $(document).ready(function () {
 });
 
 /********************** Extras **********************/
-
+//Domnita Balasa: 44.4282532,26.1001835
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 44.4282532, lng: 26.1001835};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 44.4282532, lng: 26.1001835};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
